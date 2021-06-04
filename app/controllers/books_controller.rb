41f89763c868ad27e4ed62548f3d9bb2d9882b57
Naru-hub <1208ny0324@gmail.com>
2021-06-04 14:05:41 +0000

    add favorite

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,11 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def show
     @book = Book.find(params[:id])
     @new_book = Book.new
+    @book_comment = BookComment.new
   end
 
   def index
@@ -22,7 +25,6 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
     unless @book.user_id == current_user.id
       redirect_to books_path
     end
@@ -31,7 +33,6 @@ class BooksController < ApplicationController
 
 
   def update
-    @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
@@ -41,7 +42,6 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
@@ -50,6 +50,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
-  
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
- 
   get 'home/about' => 'homes#about'
+
+  resources :users,only: [:show,:index,:edit,:update]
+
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+
 end
